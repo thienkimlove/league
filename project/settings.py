860d16ec2880db54eeba144d55cc53a3d8205217
quandm <quan.dm@teko.vn>
2018-04-10 09:55:17 +0700

    update

--- project/settings.py
+++ project/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
